6d0773cbbd47807f3ee3d3b50539cc3ff314eb15
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-04-04 13:49:43 +0200

    feat: set gaming room devices to private-network, add ollama to firewall

---
+++ fishtank/scripts/gaming-room/network.ps1
@@ -0,0 +1,2 @@
+# Set network mode to Private
+Set-NetConnectionProfile -InterfaceAlias "Ethernet" -NetworkCategory Private
--- fishtank/scripts/gaming-room/ollama.ps1
+++ fishtank/scripts/gaming-room/ollama.ps1
@@ -1,3 +1,13 @@
 [Environment]::SetEnvironmentVariable("OLLAMA_HOST", "0.0.0.0:11434", "Machine")
 choco install ollama -y
 $env:OLLAMA_HOST = "0.0.0.0:11434"
+
+# Add 11434 to the firewall for both private and public networks
+$firewallRuleName = "Ollama Port 11434"
+$firewallRule = Get-NetFirewallRule -DisplayName $firewallRuleName -ErrorAction SilentlyContinue
+if (-not $firewallRule) {
+    New-NetFirewallRule -DisplayName $firewallRuleName -Direction Inbound -Action Allow -Protocol TCP -LocalPort 11434 -Profile Any
+    Write-Output "Firewall rule '$firewallRuleName' created for port 11434."
+} else {
+    Write-Output "Firewall rule '$firewallRuleName' already exists. Skipping creation."
+}
