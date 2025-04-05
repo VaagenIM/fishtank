5b9b1144a1aac5cbe746d21d11c3f53dd9969959
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-04-05 09:46:10 +0200

    feat: open port for icmp

--- fishtank/scripts/gaming-room/network.ps1
+++ fishtank/scripts/gaming-room/network.ps1
@@ -1,2 +1,5 @@
 # Set network mode to Private
 Set-NetConnectionProfile -InterfaceAlias "Ethernet" -NetworkCategory Private
+
+# Open the port for "ping"
+New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Direction Inbound -Protocol ICMPv4 -Action Allow -LocalPort Any -Profile Any
