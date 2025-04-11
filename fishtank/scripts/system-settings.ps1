fa013b87b503a1ffc9ecccb89b63c2663fa63da3
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-04-11 17:07:05 +0200

    Update system-settings.ps1

--- fishtank/scripts/system-settings.ps1
+++ fishtank/scripts/system-settings.ps1
@@ -1,25 +1,25 @@
-# Set keyboard layout to Norwegian
-Set-WinUserLanguageList -LanguageList en-US, nb-NO -Force
+# Set the system locale to English (US)
+Set-WinSystemLocale en-US
+
+# Set the display language to English (US)
 Set-WinUILanguageOverride -Language en-US
-Set-WinUserLanguageOverride -Language en-US
 
-# Set input method (keyboard layout) to Norwegian
-$LangList = Get-WinUserLanguageList
-$LangList[0].InputMethodTips.Add("0414:00000414")  # Norwegian input method
-Set-WinUserLanguageList $LangList -Force
-
-# Set country to Norway
+# Set country/region to Norway
 Set-WinHomeLocation -GeoId 177  # 177 = Norway
 
-# Set system locale to English (US)
-Set-WinSystemLocale -SystemLocale en-US
+# Set the input method (keyboard layout) to Norwegian
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Add("nb-NO") # Add Norwegian language (for display purposes)
+Set-WinUserLanguageList $LanguageList -Force
 
-# Set timezone to Europe/Oslo
+# Set the time zone to Europe/Oslo
 Set-TimeZone -Id "W. Europe Standard Time"
 
-# Set display language to English (US)
-Set-WinUILanguageOverride -Language en-US
-Set-WinUserLanguageOverride -Language en-US
+# Set the keyboard layout to Norwegian
+$LanguageList = Get-WinUserLanguageList
+$LanguageList[0].InputMethodTips.Clear() # Clear any existing input methods
+$LanguageList[0].InputMethodTips.Add("0409:00000414")  # Norwegian Keyboard Layout
+Set-WinUserLanguageList $LanguageList -Force
 
 # Notify user
 Write-Host "Language, keyboard, region, and timezone settings have been updated."
